288f4d829eada0de65d3be2c71cef4e01e4f0442
26ANSH <anshvidyabhanu8@gmail.com>
2021-10-28 23:43:30 +0530

    ok
    Co-Authored-By: Maanas <55449222+Maanas74@users.noreply.github.com>

--- student/views.py
+++ student/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'student/index.html', {'say':' Pathshaala ❤️ Student'})
+    return render(request, 'student/say.html', {'say':' Pathshaala ❤️ Student'})
 
 def indexok(request):
     return HttpResponse("Hello, world. You're at the Student at ok index.")
--- teacher/views.py
+++ teacher/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'teacher/t.html', {'say':' Pathshaala ❤️ Teacher'})
+    return render(request, 'student/say.html', {'say':' Pathshaala ❤️ Teacher'})
 
 # Create your views here.
