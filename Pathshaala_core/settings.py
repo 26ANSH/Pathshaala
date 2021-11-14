ec7f24ad6bcc1605bdca4440b2fa95d84e592f41
26ANSH <anshvidyabhanu8@gmail.com>
2021-11-15 03:08:47 +0530

    added firebase storage support

--- Pathshaala_core/settings.py
+++ Pathshaala_core/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- teacher/firebase.py
+++ teacher/firebase.py
@@ -33,11 +33,18 @@ def teacher_login(email, password):
 # ok = storage.child("display_images/teachers/"+"default_male.png").put("student/templates/teacher/dashboard/male.png.tiff")
 # print(storage.child("display_images/teachers/"+"default_male.png").get_url())
 
-
-
+def uploadimage(fr, to):
+  location = storage.child(to)
+  location.put(fr)
+  return storage.child(to).get_url()
 
 
 # user = auth.sign_in_with_email_and_password('anshvidyabhanu8@gmail.com', 'password1234@')
 # print(user['localId'])
 # ok = auth.send_email_verification(user['idToken'])
 # print(ok)
+
+
+# print(uploadimage("media/Orange Yellow Black Illustrated Silhouette Dussehra Instagram Post.png", "display_images/courses/Orange Yellow Black Illustrated Silhouette Dussehra Instagram Post.png"))
+
+
